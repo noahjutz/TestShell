e721626b40c77f0d40ccf8d56ef91ba5157d86f2
Noah Jutz <noah.jutz@gmail.com>
2020-08-29 13:30:44 +0200

    Add basic layout

--- app/src/main/java/com/example/testshell/MainActivity.kt
+++ app/src/main/java/com/example/testshell/MainActivity.kt
@@ -4,18 +4,48 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme(colors = if (isSystemInDarkTheme()) darkColors() else lightColors()) {
-                Text("Android")
+                Content()
             }
         }
+    }
+}
+
+@Composable
+fun Content() {
+    Scaffold(topBar = { TopAppBar(title = { Text("TestShell") }) }) {
+        Column(
+                horizontalGravity = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth().fillMaxHeight()
+        ) {
+            Button(
+                    content = { Text("Click me!") },
+                    onClick = {}
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+fun ContentPreview() {
+    MaterialTheme {
+        Content()
     }
 }
