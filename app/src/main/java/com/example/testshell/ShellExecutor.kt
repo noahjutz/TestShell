3c09dd29e4d5f387773e06a8ef2cb9b04a0143c8
Noah Jutz <noah.jutz@gmail.com>
2020-08-29 13:50:42 +0200

    Make ShellExecutor even more concise

--- app/src/main/java/com/example/testshell/ShellExecutor.kt
+++ app/src/main/java/com/example/testshell/ShellExecutor.kt
@@ -1,20 +1,10 @@
 package com.example.testshell
 
-import android.util.Log
-
 object ShellExecutor {
-    private const val TAG = "ShellExecutor"
-
     fun execute(command: String) = StringBuilder().apply {
-        try {
-            Runtime.getRuntime().exec(command).apply {
-                waitFor()
-                inputStream.reader().forEachLine {
-                    append(it)
-                }
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
+        Runtime.getRuntime().exec(command).apply {
+            waitFor()
+            inputStream.reader().forEachLine { append(it) }
         }
     }.toString()
 }
