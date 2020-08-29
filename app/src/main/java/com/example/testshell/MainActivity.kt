5278dacf5f52ce975249fc0bf60bf5a265672855
Noah Jutz <noah.jutz@gmail.com>
2020-08-29 14:04:17 +0200

    Allow for entering custom command

--- app/src/main/java/com/example/testshell/MainActivity.kt
+++ app/src/main/java/com/example/testshell/MainActivity.kt
@@ -2,16 +2,20 @@ package com.example.testshell
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Icon
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 
@@ -28,18 +32,29 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun Content() {
-    val result = remember { mutableStateOf("") }
+    val input = remember { mutableStateOf("") }
+    val result = remember { mutableStateOf("No output yet :(") }
     Scaffold(topBar = { TopAppBar(title = { Text("TestShell") }) }) {
         Column(
             horizontalGravity = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
             modifier = Modifier.fillMaxWidth().fillMaxHeight().padding(24.dp)
         ) {
-            Button(
-                content = { Text("Click me!") },
-                onClick = { result.value = ShellExecutor.execute("uname -a") },
+            Row(
+                verticalGravity = Alignment.CenterVertically,
                 modifier = Modifier.padding(bottom = 16.dp)
-            )
+            ) {
+                OutlinedTextField(
+                    value = input.value,
+                    onValueChange = {input.value = it},
+                    label = { Text("Command") },
+                    modifier = Modifier.padding(end = 16.dp)
+                )
+                IconButton(
+                    icon = { Icon(Icons.Filled.Send) },
+                    onClick = { result.value = ShellExecutor.execute(input.value) },
+                )
+            }
             Text(result.value)
         }
     }
--- app/src/main/java/com/example/testshell/ShellExecutor.kt
+++ app/src/main/java/com/example/testshell/ShellExecutor.kt
@@ -2,9 +2,13 @@ package com.example.testshell
 
 object ShellExecutor {
     fun execute(command: String) = StringBuilder().apply {
-        Runtime.getRuntime().exec(command).apply {
-            waitFor()
-            inputStream.reader().forEachLine { append(it) }
+        try {
+            Runtime.getRuntime().exec(command).apply {
+                waitFor()
+                inputStream.reader().forEachLine { append(it) }
+            }
+        } catch (e: IllegalArgumentException) {
+            append("Error")
         }
     }.toString()
 }
